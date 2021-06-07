e240fa7c2ecf6ed595c230e646be4fb2c5018afd
azerpas <xskylix@hotmail.fr>
2021-06-07 12:09:00 +0200

    Started working on gradient

---
+++ src/components/gradient/index.tsx
@@ -0,0 +1,67 @@
+import { css, keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+type GradientProps = {number: number,colorFrom: string, colorTo: string, content: string, children: React.ReactNode, top?: string}
+
+const fade_1 = keyframes`
+    0%,16.667%,to {
+        opacity: 1;
+    }
+    33.333%,83.333% {
+        opacity:0;
+    }
+`;
+
+const fade_2 = keyframes`
+    0%,to {
+        opacity: 0;
+    }
+    33.333%,50% {
+        opacity: 1;
+    }
+    16.667%,66.667% {
+        opacity: 0;
+    }
+`;
+
+const fade_3 = keyframes`
+    0%,50%,to {
+        opacity: 0;
+    }
+    66.667%,83.333% {
+        opacity: 1;
+    }
+`;
+
+const faded = [fade_1,fade_2,fade_3];
+
+const Gradient = styled.span<GradientProps>`
+    position: absolute;
+    left: 0;
+    top: ${props => props.top ? `${props.top}` : `2px`};
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-image: ${props => `linear-gradient(90deg,${props.colorFrom},${props.colorTo});`};
+    -webkit-animation: ${props => css`${faded[props.number]} 8s infinite`};
+    animation: ${props => css`${faded[props.number]} 8s infinite`};
+    &::before{
+        content: ${props => props.content};
+    }
+`;
+
+const GradientBefore = styled.span<{content: string}>`
+    position: relative;
+    &:before{
+        content: "${props => props.content}";
+        position: inherit;
+    }
+`;
+
+export const GradientWrapper = (props: GradientProps) => {
+    return(
+        <GradientBefore content={props.content}>
+            <Gradient {...props}/>
+        </GradientBefore>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,15 @@ import {
 import { Container } from '../components/Container'
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
 import { Footer } from '../components/Footer'
+import { GradientWrapper } from '../components/gradient'
 import Header from '../components/header'
 
 const Index = () => (
     <Container height="100vh">
         <Header/>
-        <Text>
-        Secure
+        <Text size="xl">
+            <GradientWrapper content="Secure" colorFrom="#805AD5" colorTo="#ED64A6" number={0}>Secure</GradientWrapper>
         </Text>
-
         <DarkModeSwitch />
         <Footer>
             <Text>Next ❤️ Chakra</Text>
