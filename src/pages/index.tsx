372d8aecc49565a9657ec4a30fe95f88e4a94c59
azerpas <xskylix@hotmail.fr>
2021-06-08 14:48:43 +0200

    WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,21 +15,21 @@ import ContractSvg from '../components/svg/contract'
 const Index = () => (
     <Container height="100vh">
         <Header/>
-        <Text fontSize="4xl" fontWeight="bold">
+        <Text fontSize={{base: "3xl", md: "6xl"}} fontWeight="bold" my={{base: "6"}}>
             <GradientWrapper content="Unique" colorFrom="#805AD5" colorTo="#ED64A6" number={0}>Unique</GradientWrapper>&nbsp;&nbsp;
             <GradientWrapper content="Secure" colorFrom="#00A3C4" colorTo="#90CDF4" number={1}>Secure</GradientWrapper>&nbsp;&nbsp;
             <GradientWrapper content="Fast" colorFrom="#48BB78" colorTo="#38B2AC" number={2}>Fast</GradientWrapper>
         </Text>
         <Container mx={{base: "5"}}>
-        <SimpleGrid columns={{sm: 1, md: 2}} spacing={3}>
-            <Box maxW={{base: "sm"}} textAlign="center">
-                <ContractSvg/>
-            </Box>
-            <Box>
-                <Heading>The future of prescriptions</Heading>
-                <Text>At Prescurity, we are trying to reinvent the medical prescription thanks to the blockchain technology. Our platform enables people to generate, use and archive prescription.</Text>
-            </Box>
-        </SimpleGrid>
+            <SimpleGrid columns={{sm: 1, md: 2}} spacing={{base: 3, md: 5}} >
+                <Box maxW={{md: "sm"}} mx="auto">
+                    <ContractSvg width="20rem"/>
+                </Box>
+                <Box mx="3">
+                    <Heading>The future of prescriptions</Heading>
+                    <Text textAlign="justify">At Prescurity, we are trying to reinvent the medical prescription thanks to the blockchain technology. Our platform enables people to generate, use and archive prescription.</Text>
+                </Box>
+            </SimpleGrid>
         </Container>
         <DarkModeSwitch />
         <Footer>
