71514e8eae834ad665cf9830ed29e3dab51bb98c
martin <martin.lenaerts@hotmail.fr>
2021-06-16 12:10:30 +0200

    init doctor dashboard page ==> maquette figma to do

---
+++ src/pages/doctorDashboard.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header";
+import {Footer} from "../components/Footer";
+import {Container} from "../components/Container";
+
+const Index = () => (
+    <>
+        <Header/>
+        <Footer/>
+    </>
+);
+
+
+export default Index;
