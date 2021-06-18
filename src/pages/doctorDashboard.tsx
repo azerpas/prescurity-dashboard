25353ff75d6e7fd87a10b1c5fa09326c10fcd16a
martin <martin.lenaerts@hotmail.fr>
2021-06-18 12:10:54 +0200

    test en cours

--- src/pages/doctorDashboard.tsx
+++ src/pages/doctorDashboard.tsx
@@ -2,6 +2,13 @@ import Header from "../components/header";
 import {Footer} from "../components/Footer";
 import {Container} from "../components/Container";
 
+
+import admin from "../utils/admin";
+
+console.log(admin.initializeApp().name);
+console.log(admin.initializeApp().auth().getUser("fYEHnHSzc3UHm22Lo6zKbfzRLs12"));
+
+
 const Index = () => (
     <>
         <Header/>
