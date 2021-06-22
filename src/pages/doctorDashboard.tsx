25e9aece7699d4c355d4af872789a49a10a1e488
martin <martin.lenaerts@hotmail.fr>
2021-06-22 18:45:24 +0200

    correction d'une erreur empêchant le  deploiement sur Vercel

--- src/pages/doctorDashboard.tsx
+++ src/pages/doctorDashboard.tsx
@@ -1,13 +1,18 @@
 import {Button, Text} from "@chakra-ui/react";
 import firebase from "../utils/client";
-
+import Header from "../components/header";
+import {Footer} from "../components/Footer";
 
 const Index = () => {
+    return (
+        <>
+        <Header/>
+
+        <Footer/>
+        </>
 
 
-
-
+    );
 };
-
 
 export default Index;
