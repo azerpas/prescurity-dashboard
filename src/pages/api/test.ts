757a0ad98ef532f5bd3c60f8d3349de9dba955c9
Azerpas <xskylix@hotmail.fr>
2021-06-22 11:36:36 +0200

    Delete test.ts

--- src/pages/api/test.ts
+++
@@ -1,9 +0,0 @@
-
-import admin from "../../utils/admin";
-
-
-export default async function handler(req, res) {
-    const query = req.query;
-    const user = await admin.auth().getUser("fYEHnHSzc3UHm22Lo6zKbfzRLs12");
-    res.end(user.email);
-}
