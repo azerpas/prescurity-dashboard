08e80da0ff0b246f6f034361b004fa95c93a36be
Amel <amel.humayun@efrei.net>
2021-06-23 19:07:55 +0200

    Page patient

--- src/pages/patient.tsx
+++ src/pages/patient.tsx
@@ -10,6 +10,8 @@ import {
     Text,
     Center,
     Square,
+    border,
+    
     
     
 
@@ -22,45 +24,59 @@ import {GradientWrapper} from '../components/gradient'
 import Header from '../components/header'
 import ContractSvg from '../components/svg/contract'
 import {Span} from "next/dist/telemetry/trace";
-//import CardPatient from "../components/CardPatient"
+import CardPatient from "../components/CardPatient"
 import { Select } from "@chakra-ui/react"
 //import { Flex, Spacer } from "@chakra-ui/react"
 import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react"
+import React from 'react'
+import { Icon } from '@chakra-ui/react'
+
 
 function Patient() {
- return(
-<Container>
-    <Header/>
-              
-    <Text font-size="20px" color="gray.500">Welcome to your Prescurity patient area </Text> 
-    
-    <Box gb="black" w="100%" p={4} color = "black"> </Box>
-    
-    <Flex color="black">
-    
-    <Center w="300px" h='500' bg="gray.100">
-    
-    <Box gb="black" w="100%" p={4} color = "black">
-    
-    <Center  h="100px" >
-    <Avatar  size='xl' alignItems="center" src="https://bit.ly/broken-link" />
-    </Center>
+    return(
+   <Container>
+       <Header/>
 
-    <Center  h="100px" >
-    <Text  font-size="lg"alignText="center">Welcome, </Text>
-    </Center>
-    <Grid h="1px" p="0em" w="16em" border='1px' ></Grid>
-    <Select placeholder="Prescriptions" >  </Select>
-    <Select placeholder="QR Code">        </Select>
+       <Text font-size="20em" color="gray.500">Welcome to your Prescurity patient area </Text> 
+       
+       <Box gb="black" w="100%" p={4} color = "black"> </Box>
+       
+       <Flex color="black">
+       
+            <Center w="19em" h='500' bg="gray.100">
+       
+                <Box gb="black" w="100%" p={4} color = "black">
+       
+                    <Center  h="0,5em" >
+                    <Avatar  size='xl' alignItems="center" src="https://bit.ly/broken-link" />
+                    </Center>
+    
+                    <Center  h="14em" >
+                    <Text  h= "12em" font-ize="lg"alignText="center">Welcome, </Text>
+                    </Center>
+        
+                    <Center  h="4rem" >
+                    <Grid p="0em"  w="18rem" border='0,06rem' ></Grid>
+                    </Center>
 
-    </Box>
-    </Center>   
-    </Flex>       
+                    <Grid p="absolute"  w="21rem" h="2,375rem" border='0,06rem' borderRadius ="0,25rem" placeholder="Prescriptions" ></Grid>
+                    <Select placeholder="Prescriptions ">  </Select>
+                    <Select placeholder="QR Code">        </Select>
+
+                    <Select icon={<ChevronRightIcon />} placeholder="Prescurity" />
+
+                </Box>
+            </Center>   
+       </Flex>       
     </Container>
 
-);
+   );
+   
+    
+   
+   }
 
- 
+   export default Patient
 
-}
-export default Patient
+   
+   
