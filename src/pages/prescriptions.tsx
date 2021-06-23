b4ae0865e6f56af2a954d4c7670a48adbf58a107
hugocp9 <hv8laptop@gmail.com>
2021-06-23 17:04:49 +0200

    page prescriptions modifiée

--- src/pages/prescriptions.tsx
+++ src/pages/prescriptions.tsx
@@ -18,8 +18,7 @@ import {
     Tr,
     Th,
     Td,
-    TableCaption,
-    propNames,
+    
 
 } from '@chakra-ui/react'
 import {Container} from '../components/Container'
@@ -31,43 +30,44 @@ import ContractSvg from '../components/svg/contract'
 import {Span} from "next/dist/telemetry/trace";
 import React from 'react'
 
-const Presc = (props: {children: React.ReactNode, date: Date, doctor: String, pharma: String}) => {
+
+
+function Datashow(props) {
     return (
-        <Tbody>
-                <Tr>
-                    <Td isDate>{props.date}</Td>
-                    <Td isString>{props.doctor}</Td>
-                    <Td isString>{props.pharma}</Td>
-                    <Td><Button colorScheme="blue">view prescription</Button></Td>
-                </Tr>
-        </Tbody>
+            <Tr>
+                <Td>Date</Td>
+                <Td>Docteur</Td>
+                <Td>Pharmacien</Td>
+                <Td><Button colorScheme="#718096">view prescription</Button></Td>
+            </Tr>
     )
 }
 
-const Prescriptions = () => (
-    <Container bg="none">
-        <Header/>
-        <Heading >Patient adress: xxx</Heading>
-        <Table size="sm" width={{base:"100vh", md:"50vh"}} margin="auto">
-            <Thead>
-                <Tr>
-                    <Th >Date</Th>
-                    <Th >Doctor's name</Th>
-                    <Th >Pharmacist name</Th>
-                    <Th > Prescrition </Th>
 
-                </Tr>
-            </Thead>
-            <Tbody>
-                <Tr>
-                    <Td isDate>XX/XX/XXXX</Td>
-                    <Td isString>XXX</Td>
-                    <Td isString>XXX</Td>
-                    <Td><Button colorScheme="blue">view prescription</Button></Td>
-                </Tr>
-        </Tbody>
+
+const Prescriptions = () => (
+    <Container height="100vh" bg="none" >
+        <Header/>
+        
+         <Container height="100vh" bg="none" marginTop="10px">
+            <Heading >Patient adress: xxx</Heading>
+            <Table size="sm" >
+                <Thead>
+                    <Tr>
+                        <Th >Date</Th>
+                        <Th >Doctor's name</Th>
+                        <Th >Pharmacist name</Th>
+                        <Th > Prescrition </Th>
+                    </Tr>
+                </Thead>
+                    <Tbody>
+                        <Datashow />
+                        <Datashow />
+                    </Tbody>
+
+                </Table>
             
-        </Table>
+        </Container>
         
     </Container>
 )
