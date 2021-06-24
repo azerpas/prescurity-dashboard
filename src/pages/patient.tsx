ccc52794b9f70a90d501bd4c61f67c44d1dae08e
Amel <amel.humayun@efrei.net>
2021-06-24 15:46:23 +0200

    fourth commit

--- src/pages/patient.tsx
+++ src/pages/patient.tsx
@@ -11,6 +11,8 @@ import {
     Center,
     Square,
     border,
+    useDisclosure,
+    
     
     
     
@@ -30,9 +32,11 @@ import { Select } from "@chakra-ui/react"
 import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react"
 import React from 'react'
 import { Icon } from '@chakra-ui/react'
-
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { Fade, ScaleFade, Slide, SlideFade } from "@chakra-ui/react"
 
 function Patient() {
+  const { isOpen, onToggle } = useDisclosure();
     return(
    <Container>
        <Header/>
@@ -43,41 +47,46 @@ function Patient() {
        
        <Flex color="black">
        
-            <Center w="19em" h='500' bg="gray.100">
+            <Center w="19em" h="500" bg="gray.100" >
        
                 <Box gb="black" w="100%" p={4} color = "black">
        
-                    <Center  h="0,5em" >
-                    <Avatar  size='xl' alignItems="center" src="https://bit.ly/broken-link" />
-                    </Center>
-    
-                    <Center  h="14em" >
-                    <Text  h= "12em" font-ize="lg"alignText="center">Welcome, </Text>
-                    </Center>
-        
-                    <Center  h="4rem" >
-                    <Grid p="0em"  w="18rem" border='0,06rem' ></Grid>
-                    </Center>
-
-                    <Grid p="absolute"  w="21rem" h="2,375rem" border='0,06rem' borderRadius ="0,25rem" placeholder="Prescriptions" ></Grid>
-                    <Select placeholder="Prescriptions ">  </Select>
-                    <Select placeholder="QR Code">        </Select>
-
+                    
+                    <Avatar  bottom= "31" left="20" size='xl' alignItems="center" src="https://bit.ly/broken-link" />
+                    <Text  top="23" left="20" font-size="lg"alignText="center">Welcome, </Text>
+                    
                     <Select icon={<ChevronRightIcon />} placeholder="Prescurity" />
-                    <Select icon={<ChevronRightIcon />} placeholder="QR Code" />
+                   
+                <Button onClick={onToggle} w="17em" icon={<ChevronRightIcon />}>QR Code</Button>
+                <Fade in={isOpen}>
+                  <Box
+                    p="2rem"
+                    color="black"
+                    mt="2"
+                    bg="white"
+                    rounded="md"
+                    h="200"
+                    
+                  >
+                    
+                    
+                    
+                  </Box>
+                </Fade>
 
                 </Box>
             </Center>   
-       </Flex>       
+       </Flex>  
+       
+            
     </Container>
 
    );
    
-    
-   
    }
 
    export default Patient
-
+   //<Select icon={<ChevronRightIcon />} placeholder="QR Code" />
+                    
    
    
