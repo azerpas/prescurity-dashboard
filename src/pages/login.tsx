cd43a2b344dad68b8af455b58a6551efe0c65e03
azerpas <xskylix@hotmail.fr>
2021-06-24 10:45:01 +0200

    Added user feedback to login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,7 @@ import {
     Flex,
     Link,
     Spacer,
-    Text, Button
+    Text, Button, Spinner
 } from '@chakra-ui/react'
 
 import {Container} from '../components/Container'
@@ -12,31 +12,46 @@ import Header from '../components/header'
 import firebase from "firebase";
 import { useRouter } from 'next/router';
 import FormLogin from "../components/form";
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 function Login() {
     const router = useRouter();
     const [success, setSuccess] = useState(false);
+    const [checking, setChecking] = useState(false);
     let email: null | string = null;
     useEffect(() => {
         const signedWithLink = async () => {
             email = window.localStorage.getItem('emailForSignIn');
-            if (!email) console.error("NO MAIL");
+            if (!email){
+                router.push('/login');
+                setChecking(false);
+            }
             try {
                 const userCredential = await firebase.auth().signInWithEmailLink(email, router.asPath);
                 console.log(userCredential);
                 window.localStorage.removeItem('emailForSignIn');
-                setSuccess(true)
+                setSuccess(true);
+                setChecking(false);
             } catch (e) {
                 console.log(e);
             }
         }
         if (firebase.auth().isSignInWithEmailLink(router.asPath)) {
+            setChecking(true);
             signedWithLink();
         }
     },[])
     
+    if(checking){
+        return(
+            <Container height="100vh">
+                <Header/>
+                <Spinner size="xl" thickness="6px" />
+            </Container> 
+        );
+    }
+
     return (
         <Container height="100vh">
             <Header/>
