55e41406f09c6e0dc3597b0b28af30461a2b98a9
Amel <amel.humayun@efrei.net>
2021-06-25 15:21:39 +0200

    Page patient

--- src/pages/patient.tsx
+++ src/pages/patient.tsx
@@ -1,23 +1,15 @@
 import {
-    Box,
-    Button,
-    chakra,
-    Flex, 
-    Grid,
-    Heading,
-    Link,
-    SimpleGrid,
-    Text,
-    Center,
-    Square,
-    border,
-    useDisclosure,
-    
-    
-    
-    
-
-
+  Box,
+  Button,
+  chakra,
+  Flex, 
+  Grid,
+  Heading,
+  Link,
+  SimpleGrid,
+  Text,
+  Center,
+  useDisclosure
 } from '@chakra-ui/react';
 import {Container} from '../components/Container';
 import {DarkModeSwitch} from '../components/DarkModeSwitch';
@@ -26,9 +18,7 @@ import {GradientWrapper} from '../components/gradient';
 import Header from '../components/header';
 import ContractSvg from '../components/svg/contract';
 import {Span} from "next/dist/telemetry/trace";
-import CardPatient from "../components/CardPatient";
 import { Select } from "@chakra-ui/react";
-//import { Flex, Spacer } from "@chakra-ui/react";
 import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react";
 import React from 'react';
 import { Icon } from '@chakra-ui/react';
@@ -37,57 +27,31 @@ import {Modal,ModalOverlay,ModalContent,ModalHeader,ModalFooter,ModalBody,ModalC
 
 function Patient() {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  return(
-   <Container>
-       <Header/>
-
-       <Text font-size="20em" color="gray.500">Welcome to your Prescurity patient area </Text> 
-       
-       <Box gb="black" w="100%" p={5} color = "black"> </Box>
-       
-       <Flex color="black">
-       
-            <Center w="19em" h="500" bg="gray.100" >
-       
-                <Box gb="black" w="100%" p={4} color = "black">
-        
-                  <Avatar bottom= "0em" left="20" size='xl' alignItems="center" src="https://bit.ly/broken-link" />
-                  
-                  <Center  h="14em" >
-                    <Text  h= "12em" font-ize="lg"alignText="center">Welcome, </Text>
-                    </Center>
-
-                   
-                  <Select icon={<ChevronRightIcon />} placeholder="Prescriptions" textAlign={[  'center' ]} bottom="130" />
-                   
-
-                  <Button onClick={onOpen} w="17em" bottom="130"> QR Code </Button>
-                  <Modal isCentered onClose={onClose} isOpen={isOpen} motionPreset="slideInBottom"  >
-                   <ModalOverlay />
-                      <ModalContent>
-                        <ModalHeader>This is your QR Code </ModalHeader>
+  return(   
+    <Container>
+      <Header/>
+        <Text mb={2} color="gray.500">Welcome to your Prescurity patient area </Text> 
+          <Center w="19em" h="500" bg="gray.100" >
+            <Box gb="black" w="100%" p={4} color = "black">
+              <Avatar bottom= "0em" left="20" size='xl' alignItems="center" src="https://bit.ly/broken-link" />
+                <Center  h="14em" >
+                  <Text  h= "12em" font-ize="lg"alignText="center">Welcome, </Text>
+                </Center>
+                      <Select icon={<ChevronRightIcon />} placeholder="Prescriptions" textAlign={[  'center' ]} bottom="130" />
+                        <Button onClick={onOpen} w="17em" bottom="130"> QR Code </Button>
+                          <Modal isCentered onClose={onClose} isOpen={isOpen} motionPreset="slideInBottom"  >
+                          <ModalContent>
+                            <ModalHeader>This is your QR Code </ModalHeader>
                             <ModalCloseButton />
-                              <ModalBody>
-           
-                              </ModalBody>
-                              <ModalFooter>
-            
-            
-                              </ModalFooter>
-                        </ModalContent>
-                    </Modal>
-
-                </Box>
-             </Center>   
-         </Flex>  
-       
-            
+                            <ModalBody>
+                            </ModalBody>
+                          </ModalContent>
+                          </Modal>
+            </Box>
+          </Center>        
     </Container>
-
   );
-   
 }
-
 export default Patient
                     
    
