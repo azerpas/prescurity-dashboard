57d64612243fe8be978a0792bfc09103e4ea367e
martin <martin.lenaerts@hotmail.fr>
2021-06-25 15:42:06 +0200

    annulation des modifs (elles sont déja faites dans main)

--- src/context/user.tsx
+++ src/context/user.tsx
@@ -2,8 +2,19 @@ import React, {createContext, useContext, useState} from 'react';
 import {User} from "../entity/User";
 
 export interface IUserContext {
-    user: User,
+    user:User,
     loggedIn: Boolean;
 }
 
 export const UserContext = createContext({} as IUserContext);
+
+export function getContext() : boolean | IUserContext{
+    const iUserContext = useContext(UserContext);
+    console.log(iUserContext);
+    if(!iUserContext){
+        return false;
+    }else return iUserContext;
+}
+
+
+
