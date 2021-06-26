a42ccf1fa52b7e339f3172cdcd66e446c2833122
hugocp9 <hv8laptop@gmail.com>
2021-06-26 15:03:37 +0200

    composant prescription

--- src/components/Prescription.tsx
+++ src/components/Prescription.tsx
@@ -1,11 +1,13 @@
 import { Tr, Td, Tbody} from "@chakra-ui/table"
 import { Text, Flex, Box } from "@chakra-ui/layout"
 import { IconButton } from "@chakra-ui/button"
+import React, { useState } from "react"
 
-export const Prescription = (props: {date: String, doctor: String, pharmacist: String}) => {
+export const Prescription = (props) => {
+
     return (
         <Tbody>
-            <Flex alignContent={{base:"center" , md:"left"}} alignItems="center" size={{base:"sm", md:"md"}} border="1px" borderColor="gray.700" borderRadius="4px" margin="0.5rem">
+            <Flex alignContent={{base:"center" , md:"left"}} alignItems="center" size={{base:"sm", md:"md"}} border="1px" borderColor="gray.700" borderRadius="4px" margin="0.2rem">
                 
                 <Td shrink="1">
                     <Tr><Text>Date</Text></Tr>
