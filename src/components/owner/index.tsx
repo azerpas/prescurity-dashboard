737ac7ccae4b3fec373fc5b52d51ba61273811ec
azerpas <xskylix@hotmail.fr>
2021-06-29 14:39:38 +0200

    Changed way to get selected address

--- src/components/owner/index.tsx
+++ src/components/owner/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useState } from "react";
 import Web3 from "web3";
 import { Contract } from "web3-eth-contract";
+import { getSelectedAddress } from "../../utils/web3";
 import Header from "../header";
 
 const Index = ({web, contrat}: {web: Web3, contrat: Contract}) => {
@@ -10,12 +11,12 @@ const Index = ({web, contrat}: {web: Web3, contrat: Contract}) => {
     const [patientAddress, setPatientAddress] = useState<string|undefined>();
     const [pharmacyAddress, setPharmacyAddress] = useState<string|undefined>();
     const setDocPatient = async () => {
-        const accounts = await web.eth.getAccounts();
-        const response2 = await contrat.methods.addDoctor(docAddress,"Henry","Chirurgien").send({from: accounts[0]});
+        const selectedAddr = await getSelectedAddress();
+        const response2 = await contrat.methods.addDoctor(docAddress,"Henry","Chirurgien").send({from: selectedAddr});
         console.log(response2);
-        const response3 = await contrat.methods.addPatient(281129375779184,patientAddress).send({from: accounts[0]});
+        const response3 = await contrat.methods.addPatient(281129375779184,patientAddress).send({from: selectedAddr});
         console.log(response3);
-        const response4 = await contrat.methods.addPharmacy(pharmacyAddress, "Pharmacie de la gare").send({from: accounts[0]});
+        const response4 = await contrat.methods.addPharmacy(pharmacyAddress, "Pharmacie de la gare").send({from: selectedAddr});
         console.log(response4);
     }
     const canSubmit = () => !docAddress || !patientAddress || !pharmacyAddress;
