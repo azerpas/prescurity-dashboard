ded38dade51487a686ab606afe93f2b5871b2344
martin <martin.lenaerts@hotmail.fr>
2021-06-30 11:30:24 +0200

    verif du type du user connecté

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,39 +7,10 @@ import {useContext, useEffect, useState} from "react";
 import {User} from "../entity/User";
 import firebase from "../utils/client";
 import {Patient} from "../entity/Patient";
-
-function onAuthStateChange(callback: (IUserContext) => void) {
-    return firebase.auth().onAuthStateChanged(async credentialUser => {
-        if (credentialUser) {
-            const accessToken = await credentialUser.getIdToken();
-            const {displayName, refreshToken, email, uid} = credentialUser;
-            // FIXME: not necessarily "Patient" v
-            var currentUser: User = null;
-            // TODO : Comment récuperer l'addresse dans le userContext ? useContext(UserContext) ne peut pas être appelé ici
-            // TODO : Ou est getUserType ?
-            //getUserType(iUserContext.selectedAddress)
-            callback({loggedIn: true, user: new Patient(email, accessToken, refreshToken, email, uid, displayName)});
-        } else {
-            callback({loggedIn: false, user: null});
-        }
-    });
-}
-
-/*const onAddressChange = ({user}: {user: User}) => {
-    window.ethereum.on('accountsChanged', (accounts: Array<string>) => {
-        if(accounts.length === 0 && !user){
-            console.info('Address changed but no user defined');
-            return;
-        }else if(accounts.length === 0 && user){
-            throw new Error("Please reconnect to your account");
-        }else if(accounts.length !== 0 && user){
-            if(accounts[0] !== user.name){
-                // TODO: disconnect user
-                throw new Error("Please reconnect to your account");
-            } 
-        }
-    });
-}*/
+import {initWeb3} from "../utils/web3";
+import {Pharmacy} from "../entity/Pharmacy";
+import {Doctor} from "../entity/Doctor";
+import {Owner} from "../entity/Owner";
 
 const onChainChange = () => {
     window.ethereum.on('chainChanged', (_chainId: string) => {
@@ -68,7 +39,28 @@ function MyApp({Component, pageProps}: AppProps) {
         });
 
 
-        const unsubscribe = onAuthStateChange(setUser);
+        const unsubscribe = firebase.auth().onAuthStateChanged(async credentialUser => {
+            if (credentialUser) {
+                const accessToken = await credentialUser.getIdToken();
+                const {displayName, refreshToken, email, uid} = credentialUser;
+                let currentUser: User = null;
+                const [web,contract] = await initWeb3();
+                const userType = await contract.methods.getUserType().call({from:user.selectedAddress});
+                console.log(userType);
+                if(userType==="patient"){
+                    currentUser = new Patient(email, accessToken, refreshToken, email, uid, displayName)
+                }else if(userType==="pharmacy"){
+                    currentUser = new Pharmacy(email, accessToken, refreshToken, email, uid, displayName)
+                }else if (userType === "doctor"){
+                    currentUser = new Doctor(email, accessToken, refreshToken, email, uid, "speciality",displayName)
+                }else if (userType === "owner"){
+                    currentUser = new Owner(email, accessToken, refreshToken, email, uid,displayName)
+                }
+                setUser({loggedIn: true, user: currentUser,selectedAddress:user.selectedAddress});
+            } else {
+                setUser({loggedIn: false, user: null,selectedAddress:user.selectedAddress});
+            }
+        });
         onChainChange();
         return () => {
             unsubscribe();
