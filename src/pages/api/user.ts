aa027a83c183fc8797029e67c0aeeceefbcf18f8
azerpas <xskylix@hotmail.fr>
2021-06-30 09:22:17 +0200

    refactoring

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -3,12 +3,8 @@ import {NextApiRequest, NextApiResponse} from "next";
 
 export default async function User(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === "GET"){
-        if (!req.query.email) {
-            res.status(401).json({
-                message: "No email sent",
-                name: "error"
-            })
-        } else {
+        if (!req.query.email) res.status(401).json({ message: "No email sent", name: "error" });
+        else {
             try {
                 await admin.auth().getUserByEmail(req.query.email.toString());
                 res.json({userExist: true});
@@ -18,9 +14,8 @@ export default async function User(req: NextApiRequest, res: NextApiResponse) {
             }
         }
     }else if(req.method === "POST"){
-        if (!req.body.email) {
-            res.status(401).json({ message: "No email sent", name: "error" })
-        } else if (!req.body.address) res.status(401).json({ message: "No address sent", name: "error" })
+        if (!req.body.email) res.status(401).json({ message: "No email sent", name: "error" }); 
+        else if (!req.body.address) res.status(401).json({ message: "No address sent", name: "error" })
         else {
             try {
                 await admin.auth().createUser({
@@ -33,11 +28,5 @@ export default async function User(req: NextApiRequest, res: NextApiResponse) {
                 res.json({userExist: true});
             }
         }
-    }else{
-        res.status(400).json({
-            message: "METHOD unhautorized",
-            name: "error"
-        })
-    }
-
+    }else res.status(400).json({ message: "Method unhautorized", name: "error" });
 }
