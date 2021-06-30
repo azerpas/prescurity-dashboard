afe44ec143b4d116e0079b605fdd47a0710ee773
martin <martin.lenaerts@hotmail.fr>
2021-06-30 12:32:46 +0200

    ajout address dans pharmacist

--- src/components/pharmacist/index.tsx
+++ src/components/pharmacist/index.tsx
@@ -1,98 +1,99 @@
 import {Flex, Heading, Text, Box} from "@chakra-ui/layout"
-import { Container } from "../../components/Container" 
-import {Footer} from '../../components/Footer'
-import { Input } from "@chakra-ui/input"
-import { Button } from "@chakra-ui/button"
+import {Container} from "../Container"
+import {Footer} from '../Footer'
+import {Input} from "@chakra-ui/input"
+import {Button} from "@chakra-ui/button"
 import Header from '../../components/header'
-import React from 'react'
+import React, {useState} from 'react'
 import Web3 from "web3";
-import { Contract } from "web3-eth-contract";
+import {Contract} from "web3-eth-contract";
+import {initWeb3} from "../../utils/web3";
 
 
-const Index = ({web, contrat}: {web: Web3, contrat: Contract}) => {
-    return(
+const Index = ({web, contrat}: { web: Web3, contrat: Contract }) => {
+    const [address, setAddress] = useState<string>("");
+
+
+   /* const searchPrescriptions = async (address) => {
+        const [web,contract] = await initWeb3();
+        contract.methods.get
+    }*/
+
+
+    const addressChange = (event) => {
+        setAddress(event.target.value)
+    }
+    return (
         <Container height="100vh" bg="none" alignItems="left">
             <Header/>
+            <Flex direction="column" margin="1rem" align="center">
+                <Heading fontSize={{base: "20px", md: "28px", lg: "40px"}} as="h2" color="#718096" fontFamily="Inter" textAlign="center">
+                    Welcome to your Prescurity pharmacist's area</Heading>
 
-            <Flex direction="column"  margin="1rem" align="center">
-                <Heading fontSize={{base:"20px" , md:"28px", lg:"40px" }} as="h2" color="#718096" fontFamily="Inter" textAlign="center">
-                Welcome to your Prescurity pharmacist's area</Heading>
-
-                <Container alignItems="left" bg="none" margin="2rem" >
-
-                    <Text fontSize={{base:"sm", md:"md"}} color="gray.700" > Patient address</Text>
-                    <Input size="md" borderRadius="6px" borderColor="gray.200"  width="20rem"/>
-                    <Text fontSize={{base:"xs", md:"sm"}} as="u"  color="gray.500">OR scan the QR code</Text> 
-
+                <Container alignItems="left" bg="none" margin="2rem">
+                    <Text fontSize={{base: "sm", md: "md"}} color="gray.700"> Patient address</Text>
+                    <Input size="md" borderRadius="6px" borderColor="gray.200" width="20rem" type={"number"} onChange={addressChange}/>
+                    <Text fontSize={{base: "xs", md: "sm"}} as="u" color="gray.500">OR scan the QR code</Text>
                 </Container>
-
             </Flex>
 
             <Container height="100vh" bg="none" alignItems="left">
-            
-            <Flex direction="column" alignItems="left" justifyContent="flex-start" margin={{base:"auto", md:"1rem"}}>
+                <Flex direction="column" alignItems="left" justifyContent="flex-start" margin={{base: "auto", md: "1rem"}}>
+                    <Text padding="1rem" fontSize={{base: "md", md: "lg"}} color="gray.700">Patient adress: {address}</Text>
+                    <Flex alignContent={{base: "center", md: "left"}} maxW="450px" alignItems="center" size={{base: "sm", md: "md"}} border="1px" borderColor="gray.700" borderRadius="4px" margin="0.2rem">
+                        <Box padding="0.5rem">
+                            <ul><Text fontWeight="bold">Date</Text></ul>
+                            <ul><Text>XX/XX/XXXX</Text></ul>
+                        </Box>
+                        <Box padding="0.5rem">
+                            <ul><Text fontWeight="bold">Doctor</Text></ul>
+                            <ul><Text>Doctor's name</Text></ul>
 
-                <Text padding="1rem" fontSize={{base:"md" , md:"lg"}} color="gray.700">Patient adress: xxxx@xxxx.com</Text>
-                
+                        </Box>
 
+                        <Box padding="0.5rem">
 
-                <Flex alignContent={{base:"center" , md:"left"}} maxW="450px" alignItems="center" size={{base:"sm", md:"md"}} border="1px" borderColor="gray.700" borderRadius="4px" margin="0.2rem">
-                    
-                    <Box padding="0.5rem">
-                        <ul><Text fontWeight="bold">Date</Text></ul>
-                        <ul><Text>XX/XX/XXXX</Text></ul>
-                    </Box>
-                    
-                    <Box padding="0.5rem">
-                        <ul><Text fontWeight="bold">Doctor</Text></ul>
-                        <ul><Text>Doctor's name</Text></ul>
-                        
-                    </Box>  
-                    
-                    <Box padding="0.5rem">
+                            <ul><Text fontWeight="bold">Pharmacist</Text></ul>
+                            <ul><Text>Pharmacist's name</Text></ul>
+                        </Box>
 
-                        <ul><Text fontWeight="bold">Pharmacist</Text></ul>
-                        <ul><Text >Pharmacist's name</Text></ul>
-                    </Box>
-                        
-                    <Button aria-label="go to file" />
+                        <Button aria-label="go to file"/>
 
-                </Flex>  
+                    </Flex>
 
-                <Flex alignContent={{base:"center" , md:"left"}} maxW="450px" alignItems="center" size={{base:"sm", md:"md"}} border="1px" borderColor="gray.700" borderRadius="4px" margin="0.2rem">
-                    
-                    <Box padding="0.5rem">
-                        <ul><Text fontWeight="bold">Date</Text></ul>
-                        <ul><Text>XX/XX/XXXX</Text></ul>
-                    </Box>
-                    
-                    <Box padding="0.5rem">
-                        <ul><Text fontWeight="bold">Doctor</Text></ul>
-                        <ul><Text>Doctor's name</Text></ul>
-                        
-                    </Box>  
-                    
-                    <Box padding="0.5rem">
+                    <Flex alignContent={{base: "center", md: "left"}} maxW="450px" alignItems="center" size={{base: "sm", md: "md"}} border="1px" borderColor="gray.700" borderRadius="4px" margin="0.2rem">
 
-                        <ul><Text fontWeight="bold">Pharmacist</Text></ul>
-                        <ul><Text >Pharmacist's name</Text></ul>
-                    </Box>
-                        
-                    <Button aria-label="go to file" />
+                        <Box padding="0.5rem">
+                            <ul><Text fontWeight="bold">Date</Text></ul>
+                            <ul><Text>XX/XX/XXXX</Text></ul>
+                        </Box>
 
-                </Flex>  
+                        <Box padding="0.5rem">
+                            <ul><Text fontWeight="bold">Doctor</Text></ul>
+                            <ul><Text>Doctor's name</Text></ul>
+
+                        </Box>
+
+                        <Box padding="0.5rem">
+
+                            <ul><Text fontWeight="bold">Pharmacist</Text></ul>
+                            <ul><Text>Pharmacist's name</Text></ul>
+                        </Box>
+
+                        <Button aria-label="go to file"/>
+
+                    </Flex>
 
 
-            </Flex>
-            
-            
-            
-        </Container>
-        
-        <Footer></Footer>
-            
+                </Flex>
+
+
+            </Container>
+
+            <Footer/>
+
         </Container>
     );
 }
-    
+
 export default Index
