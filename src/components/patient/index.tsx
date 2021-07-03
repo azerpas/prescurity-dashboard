aa958eba89d0cee4a088ce0076d303b889fc735a
Amel <amel.humayun@efrei.net>
2021-07-03 16:32:28 +0200

    Patient component

--- src/components/patient/index.tsx
+++ src/components/patient/index.tsx
@@ -22,11 +22,11 @@ const Index = ({web, contrat}: {web: Web3, contrat: Contract}) => {
                     <Text mb={2} color="gray.500">Welcome to your Prescurity patient area </Text> 
                         <Center w="19em" h="500" bg="gray.100" >
                             <Box gb="black" w="100%" p={4} color = "black">
-                            <Avatar bottom= "0em" left="20" size='xl' alignItems="center" src="https://bit.ly/broken-link" />
-                            <Center  h="14em" >
+                            <Avatar bottom= "0em" left="20" size='xl' alignItems="center" src="https://bit.ly/broken-link"/>
+                            <Center  h="14em">
                             <Text  h= "12em" font-ize="lg"alignText="center">Welcome, </Text>
                             </Center>
-                            <Select icon={<ChevronRightIcon />} placeholder="Prescriptions" textAlign={[  'center' ]} bottom="130" />
+                            <Select icon={<ChevronRightIcon />} placeholder="Prescriptions" textAlign={[  'center' ]} bottom="130"/>
                             </Box>
                         </Center>        
             </Container>
