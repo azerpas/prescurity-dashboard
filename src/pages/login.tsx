4f6d7894651d3b0d9d61cc0ff8d4a94e2732a537
martin <martin.lenaerts@hotmail.fr>
2021-07-05 17:52:56 +0200

    ajout toast login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,20 +3,20 @@ import {
     Divider,
     Flex,
     Spacer,
-    Text, Button, Spinner,
+    Text, Button, Spinner, Alert, AlertIcon, AlertTitle, AlertDescription, useToast,
 } from '@chakra-ui/react'
 import Link from "next/link";
 import {Container} from '../components/Container'
 import Header from '../components/header'
 import firebase from "firebase";
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 import FormLogin from "../components/form";
 import React, {useEffect} from 'react';
-import { useState } from 'react';
-import { PresLink } from '../components/link';
-import { useContext } from 'react';
-import { AlertContext } from '../context/alert';
-import { MajorAlert } from '../components/alert';
+import {useState} from 'react';
+import {PresLink} from '../components/link';
+import {useContext} from 'react';
+import {AlertContext} from '../context/alert';
+import {MajorAlert} from '../components/alert';
 import * as ROUTES from '../constants/routes';
 
 function Login() {
@@ -24,11 +24,12 @@ function Login() {
     const [success, setSuccess] = useState(false);
     const [checking, setChecking] = useState(false);
     const alertContext = useContext(AlertContext);
+    const toast = useToast();
     let email: null | string = null;
     useEffect(() => {
         const signedWithLink = async () => {
             email = window.localStorage.getItem('emailForSignIn');
-            if (!email){
+            if (!email) {
                 await router.push(ROUTES.LOGIN);
                 setChecking(false);
             }
@@ -39,25 +40,32 @@ function Login() {
                 setSuccess(true);
                 setChecking(false);
             } catch (e) {
-                console.log(e);
+                toast({
+                    title: "Login error",
+                    description: e.message,
+                    status: "error",
+                    duration: 9000,
+                    isClosable: true,
+                });
+                setChecking(false);
             }
         }
         if (firebase.auth().isSignInWithEmailLink(router.asPath)) {
             setChecking(true);
             signedWithLink();
         }
-    },[])
-    
-    if(checking){
-        return(
+    }, [])
+
+    if (checking) {
+        return (
             <Container height="100vh">
                 <Header/>
-                <Spinner size="xl" thickness="6px" />
-            </Container> 
+                <Spinner size="xl" thickness="6px"/>
+            </Container>
         );
     }
 
-    if(success){
+    if (success) {
         router.push(ROUTES.DASHBOARD);
         return <></>;
     }
@@ -65,13 +73,13 @@ function Login() {
     return (
         <Container height="100vh">
             <Header/>
-            {alertContext.title ? 
+            {alertContext.title ?
                 <MajorAlert {...alertContext}/>
-            : <></>}
+                : <></>}
             <Container my="4rem" minH={{base: "55vh", sm: "60vh", md: "60vh"}}>
                 <Heading>Login to Prescurity</Heading>
 
-                { success ? 
+                {success ?
                     <>
                         <Text>Your are signing in with {email} </Text>
                         <Link href={ROUTES.DASHBOARD}><a><Button>Go to the dashboard</Button></a></Link>
@@ -79,15 +87,15 @@ function Login() {
                     :
                     <>
                         <FormLogin/>
-                        { !success &&
-                            <>
-                                <Divider mt="2em" borderColor="gray.600"/>
-                                <Flex mt="1em"><PresLink href={"/join"}><a>Don't have an account:&nbsp;Register Here</a></PresLink></Flex>
-                            </>
+                        {!success &&
+                        <>
+                            <Divider mt="2em" borderColor="gray.600"/>
+                            <Flex mt="1em"><PresLink href={"/join"}><a>Don't have an account:&nbsp;Register Here</a></PresLink></Flex>
+                        </>
                         }
                     </>
                 }
-                
+
             </Container>
         </Container>
     );
