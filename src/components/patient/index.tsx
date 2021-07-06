7aa25d6c3b8d36f78175bc1bf5b05dfd07e07c88
azerpas <xskylix@hotmail.fr>
2021-07-06 18:20:44 +0200

    Cleared patient

--- src/components/patient/index.tsx
+++ src/components/patient/index.tsx
@@ -1,9 +1,9 @@
 import React, {useContext, useEffect, useState} from "react";
-import {Avatar, Button, Center, Container, Text, useClipboard} from "@chakra-ui/react";
+import {Avatar, Button, Center, Container, InputGroup, InputRightElement, Text, useClipboard} from "@chakra-ui/react";
 import Web3 from "web3";
 import {Contract} from "web3-eth-contract";
 import Header from "../header";
-import {ArrowForwardIcon} from "@chakra-ui/icons";
+import {ArrowDownIcon, ArrowForwardIcon} from "@chakra-ui/icons";
 import {Flex, Heading} from "@chakra-ui/layout";
 import {Prescription} from "../../entity/Prescription";
 import {UserContext} from "../../context/user";
@@ -17,7 +17,6 @@ function randomDate(start, end) {
 
 const Index = ({web, contrat}: { web: Web3, contrat: Contract }) => {
     const [prescriptionSelected, setPrescriptionSelected] = useState(false)
-    const [NumsecuSelected, setNumsecuSelected] = useState(false)
     const [prescriptions, setPrescriptions] = useState<Prescription[]>([]);
     const context = useContext(UserContext);
     const value = context.user.uid
@@ -41,69 +40,49 @@ const Index = ({web, contrat}: { web: Web3, contrat: Contract }) => {
     return (
         <>
             <Header/>
-            <Heading mb={"2rem"} textAlign={"center"} color="gray.500" fontSize={{base: "25px", md: "32px", lg: "48px"}}>Welcome to your Prescurity patient area </Heading>
-            <Flex flexWrap="wrap">
-                <Flex mx={"auto"} bg="gray.100" p={"2rem"} w={[250, 350, 500]} alignSelf={"flex-start"} flexDirection="column" mt={"2rem"}>
-                    <Avatar size='xl' src="https://bit.ly/broken-link" m={"auto"}/>
-                    <Text fontSize={"lg"} align="center" mb={"2rem"}>Welcome {context.user.name} </Text>
-                    <Button mb={"2rem"} border={"1px grey solid"} isActive={prescriptionSelected} rightIcon={<ArrowForwardIcon/>} onClick={() => {
-                        setPrescriptionSelected(true), setNumsecuSelected(false)
-                    }}>
-                        <Text m={'auto'}>Prescriptions</Text>
-                    </Button>
-                    <Button style={{whiteSpace: "normal", wordWrap: "break-word"}} mb={"2rem"} border={"1px grey solid"} isActive={!prescriptionSelected} rightIcon={<ArrowForwardIcon/>} onClick={() => {
-                        setPrescriptionSelected(false), setNumsecuSelected(true)
-                    }}>
-                        <Text m={'auto'}>Numéro de sécurité sociale</Text>
-                    </Button>
-                </Flex>
-                {
-                    prescriptionSelected ?
-                        <>
-                            <Container>
-                                <Heading fontSize={"xl"} align="center" mb={"2rem"} mt={"2rem"}> Prescriptions</Heading>
-                                <Flex direction="column" alignItems="left" justifyContent="flex-start" margin={{base: "auto", md: "1rem"}}>
-                                    <Container>
-                                        {
-                                            prescriptions.map((prescription: Prescription) => {
-                                                return <CardPrescription contrat={contrat} prescription={prescription}/>
-                                            })
-                                        }
-                                    </Container>
-                                </Flex>
-                            </ Container>
-                        </>
-                        :
-                        <>
-                        </>
-
-
-                }
-                {
-                    NumsecuSelected ?
-                        <>
-                            <Container>
-                                <Heading fontSize={"xl"} align="center" mb={"2rem"} mt={"2rem"}> Numéro de sécurité sociale</Heading>
-                                <Flex direction="column" align="center" margin={{base: "auto", md: "1rem"}}>
-                                    <Text mt="5rem">Your security social number :</Text>
-                                    <Flex m={"auto"} textAlign={"center"} alignContent={"baseline"} my={"2rem"}>
-                                        <Input isReadOnly alignSelf={"center"} value={context.user.uid}/>
-                                        <Button maxW={"min-content"} alignSelf={"center"} ml={"0.5rem"} onClick={onCopy}> {hasCopied ? "Copied" : "Copy"} </Button>
+            <Container>
+                <Flex flexWrap="wrap" w="100%">
+                    <Flex 
+                        mx={"auto"} px={2} py={2} w="100%"
+                        borderColor="blackAlpha.200" borderWidth={1} borderRadius={5}
+                        alignSelf={"flex-start"} flexDirection="column"
+                    >
+                        <Heading mx="0.5" textAlign={"center"}>Welcome</Heading>
+                        <Avatar size='xl' src="https://bit.ly/broken-link" m={"auto"}/>
+                        <Text fontSize={"lg"} align="center">{context.user.name} </Text>
+                        <Flex direction="column" align="center" w="100%" my="4">
+                            <Text fontSize="sm" textAlign="center">Security social number:</Text>
+                            <Flex w="100%" textAlign={"center"} alignContent={"baseline"} >
+                                <InputGroup size="md">
+                                    <Input isReadOnly alignSelf={"center"} value={context.user.uid} pr="4rem"/>
+                                    <InputRightElement w="4rem">
+                                        <Button size="sm"  alignSelf={"center"} onClick={onCopy}> {hasCopied ? "Copied" : "Copy"} </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </Flex>
+                        </Flex>
+                        <Button mb={"2rem"} border={"1px grey solid"} isActive={prescriptionSelected} rightIcon={!prescriptionSelected ? <ArrowForwardIcon/> : <ArrowDownIcon/>} onClick={() => setPrescriptionSelected(!prescriptionSelected) }>
+                            <Text m={'auto'}>Prescriptions</Text>
+                        </Button>
+                        {
+                            prescriptionSelected ?
+                            <>
+                                    <Flex direction="column" alignItems="left" justifyContent="flex-start">
+                                            {
+                                                prescriptions.map((prescription: Prescription) => {
+                                                    return <CardPrescription contrat={contrat} prescription={prescription}/>
+                                                })
+                                            }
                                     </Flex>
-                                    <Container mt="5rem">
-                                        <Center>
-
-
-                                        </Center>
-                                    </Container>
-                                </Flex>
-                            </ Container>
-                        </>
-                        :
-                        <>
-                        </>
-                }
-            </Flex>
+                            </>
+                            :
+                            <>
+                            </>
+                        }
+                    </Flex>
+                    
+                </Flex>
+            </Container>
         </>
     )
 }
